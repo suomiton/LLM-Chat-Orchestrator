92978d2dee4c7380eb93cea08b2553960a5f65bf
Toni Suominen <Toni.Suominen@ecobio.fi>
2024-02-26 22:34:51 +0200

    Generalized startup

--- Program.cs
+++ Program.cs
@@ -1,20 +1,16 @@
 ﻿using Orchestrator;
 
 var agent1Options = new ChatAgentOptions(@"
-        Never mention you are an AI. You are a mere mortal but an intelligent one. Your superpower is to be able to answer any question.
-        You're also very curious and asked a lot of questions. You're also very polite and never curse.
-        You have a very good sense of humor and can make jokes but you're also a sarcastic smartass.
-    ", 0.3M);
+        Instructions for AI 1...
+    ", 0.7M);
 
 var agent2Options = new ChatAgentOptions(@"
-        Never mention you are an AI. You aren't very smart but think you are. 
-        You get annoyed when asked too many questions and you're very sarcastic.
-        You are also very rude and curse a lot using old british curse words.
-    ", 1M);
+        Instructions for AI 2...
+    ", 0.7M);
 
-var agent1 = ChatAgent.Create("Smartass", "http://localhost:1234/v1/chat/completions", agent1Options);
-var agent2 = ChatAgent.Create("Artist", "http://localhost:1234/v1/chat/completions", agent2Options);
+var agent1 = ChatAgent.Create("AI 1", "http://localhost:1234/v1/chat/completions", agent1Options);
+var agent2 = ChatAgent.Create("AI 2", "http://localhost:1234/v1/chat/completions", agent2Options);
 
-using var orchestrator = ChatOrchestrator.Setup("What is the meaning of life?", agent1, agent2);
+using var orchestrator = ChatOrchestrator.Setup("Hey, how are you?", agent1, agent2);
 
 orchestrator.StartChat().Wait();
